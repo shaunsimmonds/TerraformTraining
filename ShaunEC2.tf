29531f4ca62874b54b09e6a07bc845422a2edb04
shaunsimmonds <simmonds.shaun@gmail.com>
2020-06-22 14:18:06 -0400

    Update ShaunEC2.tf

--- ShaunEC2.tf
+++ ShaunEC2.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "AKIAW7LCT4LO35TSA7YV"
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" myec2 {
   ami = "ami-082b5a644766e0e6f"
   instance_type = t2.micro"
   }
