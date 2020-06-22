d804ed665b4029fc60519c1dfc46a0f7201aa8ee
shaunsimmonds <simmonds.shaun@gmail.com>
2020-06-22 14:40:21 -0400

    Update ShaunEC2.tf

--- ShaunEC2.tf
+++ ShaunEC2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAW7LCT4LO35TSA7YV"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2" {
   ami = "ami-082b5a644766e0e6f"
   instance_type = "t2.micro"
