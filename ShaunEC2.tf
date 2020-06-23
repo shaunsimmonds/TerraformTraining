2a6d6da531a47fa77ddb4b3936658f65d2c174b8
shaunsimmonds <simmonds.shaun@gmail.com>
2020-06-23 13:05:24 -0400

    Update ShaunEC2.tf

--- ShaunEC2.tf
+++ ShaunEC2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-west-2"
 }
 
-resource "aws_instance" "ShaunTestEC2" {
+resource "aws_instance" "myec2" {
    ami = "ami-0a54aef4ef3b5f881"
    instance_type = "t2.micro"
 }
